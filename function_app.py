eee964cda237114724b08fa98208293f80ac5308
BsoftLimited <okelekelenobel@gmail.com>
2025-04-22 14:41:19 +0000

    I am a boy with cryptograph fix

--- function_app.py
+++ function_app.py
@@ -25,7 +25,6 @@ def mytrigger(req: func.HttpRequest) -> func.HttpResponse:
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
-        return func.HttpResponse(
-             f.decrypt(token) + "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+        return func.HttpResponse("This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
